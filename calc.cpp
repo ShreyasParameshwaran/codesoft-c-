4dc0eaed5b513d4f484d5c40b8d09c25aa13a8a4
ShreyasParameshwaran <116908037+ShreyasParameshwaran@users.noreply.github.com>
2023-11-16 20:29:52 +0530

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main()
 {   
+	system("cls");
 	char op;
 	float num1, num2;
 
-    cout<<"Enter op ";
+	int i = 0;
+
+	while (i<20)
+	{	
+    cout<<"Enter 2 numbers \n";	
+	cin >> num1 >> num2;
+
+	cout<<"Enter operation for calculator \n + for addition \n - for subtraction \n / for division \n *for multiplication\n";
 	cin >> op;
-
-    cout<<"Enter 2 numbers "
-;	cin >> num1 >> num2;
-
-
 	switch (op) {
 
 	case '+':
@@ -37,7 +40,8 @@ int main()
 
 	default:
 		cout << "Error! operator is not correct";
-	}
+		i++;
+	}}
 
 
 	return 0;
